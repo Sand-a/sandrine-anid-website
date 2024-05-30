e85424917665c8874d55739465c1716053350b40
Sand-a <s_anid@hotmail.com>
2024-05-30 14:50:42 +0100

    emailjs

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "./Contact.css";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_lqqqku2",
+        "template_l7cfa85",
+        form.current,
+        "dTBSZ2Eq7M3l7T5WI"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
     <div id="contact" className="section section--4 container section-contact">
       <h1 className="primary-headline contact-headline contact-headline-1">
@@ -10,7 +33,7 @@ const Contact = () => {
       <h1 className="primary-headline contact-headline contact-headline-2">
         Drop me a Line !
       </h1>
-      <form className="contact-form">
+      <form ref={form} onSubmit={sendEmail} className="contact-form">
         <div className="form-group">
           <label for="name" className="bodytext">
             Your Name
@@ -30,7 +53,7 @@ const Contact = () => {
           <input
             className="searchfield"
             type="text"
-            placeholder="email"
+            placeholder="Email"
             name="user_email"
             required
           />
