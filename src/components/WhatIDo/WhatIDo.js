508c23826f1e16a49e7031e6a519dcd999e62e8f
Sand-a <s_anid@hotmail.com>
2024-05-31 11:43:41 +0100

    go back

--- src/components/WhatIDo/WhatIDo.js
+++ src/components/WhatIDo/WhatIDo.js
@@ -6,17 +6,7 @@ import Card from "../Card/Card";
 const WhatIDo = ({ cardsData }) => {
   return (
     <>
-      <motion.section
-        initial={{ opacity: 0, y: 200 }}
-        whileInView={{
-          opacity: 1,
-          y: 0,
-          transition: { duration: 0.4 },
-        }}
-        viewport={{ once: true, amount: 0.5 }}
-        id="about"
-        className="section section--1 what-i-do-section"
-      >
+      <div id="about" className="section section--1 what-i-do-section">
         <h1 className="primary-headline what-i-do-headline">What i do</h1>
 
         <div className="container cards-container">
@@ -24,7 +14,7 @@ const WhatIDo = ({ cardsData }) => {
             return <Card card={card} />;
           })}
         </div>
-      </motion.section>
+      </div>
     </>
   );
 };
