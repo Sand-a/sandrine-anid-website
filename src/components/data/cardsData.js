73e55acd92ff3436399319c104a985160de629ea
Sand-a <s_anid@hotmail.com>
2024-06-03 17:31:28 +0100

    about text

--- src/components/data/cardsData.js
+++ src/components/data/cardsData.js
@@ -4,7 +4,7 @@ export const cardsData = [
     icon: "assets/logo/s-logo-black.svg",
     title: "UI | UX design.",
     slogan: "Image is Everything.",
-    text: "From the first impression when visiting your website to how users navigate through it, I create meaningful digital experiences tailored to your users' needs. I start with an appealing User Interface, crafting a distinctive identity and using attractive visuals to showcase your services. This includes designing logos, customizing icons and illustrations, selecting color schemes, refining photos, and ensuring that your users enjoy a seamless journey by providing the best possible User Experience.\n\nI bring my designs to life using:",
+    text: "From the first impression when visiting your website to how users navigate through it, I create meaningful digital experiences tailored to your users' needs. I start with an appealing User Interface, crafting a distinctive identity and using attractive visuals to showcase your services. This includes designing logo, customizing icons and illustrations, selecting color schemes, refining photos... Lastly, I will ensure that your users enjoy a seamless journey by designing prototypes and conducting tests to provide the best possible User Experience.\n\nI bring my designs to life using:",
     skills: "Figma, Illustrator & Photoshop.",
   },
   {
@@ -12,7 +12,7 @@ export const cardsData = [
     icon: "assets/logo/s-logo-black-flip.svg",
     title: "Frontend Development.",
     slogan: "Optimization & Performance are Keys.",
-    text: "I build websites from scratch, translating designs into clean, organized, efficient, and scalable code. My focus is on optimizing websites for top performance, usability, and responsiveness across various operating systems and screen sizes, from mobile and tablet to desktop. \nMy services include meticulous debugging, problem-solving, and maintenance to ensure sustained functionality.\n\n I will bring your vision to life by translating my designs into code using my dedicated development tools:",
+    text: "I build websites from scratch, translating designs into clean, organized, efficient, and scalable code. My focus is on optimizing websites for top performance, usability, and responsiveness across various operating systems and screen sizes, spanning from mobile and tablet to desktop. Up-to-date on tech and coding, I challenge myself by integrating captivating SVG animations, to set your site apart. My services include meticulous debugging, problem-solving, and maintenance to ensure sustained functionality.\n\n I will bring your vision to life by translating designs into code using my dedicated development tools:",
     skills: "HTML, CSS, JavaScript & React JS.",
   },
 ];
